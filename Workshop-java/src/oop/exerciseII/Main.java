76430e603423493be26df47458fb5fbc86f1f6f9
Babiuc Ghenadie <babiuc.ghenadii@gmail.com>
2023-01-15 20:05:07 +0200

    oop Exercise #2

---
+++ Workshop-java/src/oop/exerciseII/Main.java
@@ -0,0 +1,50 @@
+package oop.exerciseII;
+
+import oop.exerciseI.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        Scanner cin = new Scanner(System.in);
+        int gender;
+
+        for (int i = 0; i < 3; i++) {
+            Person person = new Person();
+            System.out.println("Introduceti numele persoanei ");
+            person.setName(cin.next());
+            System.out.println("Introduceti data nasterii in format dd/MM/yyyy");
+            person.setYearOfBirth(cin.next());
+            do {
+                System.out.println("Alegeti genul: 1 pentru Masculin si 2 pentru feminin ");
+                gender = cin.nextInt();
+            } while (gender != 1 && gender != 2);
+            if (gender == 1) {
+                person.setGender("M");
+            } else {
+                person.setGender("F");
+            }
+            System.out.println("Introduceti inaltimea in cm");
+            person.setHeight(cin.nextInt());
+            people.add(person);
+        }
+
+
+        System.out.println("--------------------Persoane cu vârsta peste 25 de ani-----------------");
+        for (Person s : people) {
+            if (s.calculateAge() > 25) {
+                System.out.println(s.toString());
+            }
+        }
+
+        System.out.println("--------------------Persoane cu greutatea cuprinsă între 55 și 80 kg-----------------");
+        for (Person s : people) {
+            if (s.calculateWeight() >= 55 && 80 <= s.calculateWeight()) {
+                System.out.println(s.toString());
+            }
+        }
+    }
+}
---
+++ Workshop-java/src/oop/exerciseII/Person.java
@@ -0,0 +1,46 @@
+package oop.exerciseII;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    private String name;
+    private String yearOfBirth;
+    private String gender;
+    private int height;
+
+    public int calculateAge() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate localDate1 = LocalDate.parse(yearOfBirth, formatter);
+        LocalDate end = LocalDate.now();
+        return (int) ChronoUnit.YEARS.between(localDate1, end);
+    }
+
+    public double calculateWeight() {
+        if (gender.equalsIgnoreCase("M")) {
+            return (double) (50 + 0.75 * (height - 150) + (calculateAge() - 20) / 4);
+        } else {
+            return (double) (50 + 0.75 * (height - 150) + (calculateAge() - 20) / 4) - 10;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", yearOfBirth='" + yearOfBirth + '\'' +
+                ", gender='" + gender + '\'' +
+                ", height=" + height + '\'' +
+                ", Age=" + calculateAge() + '\'' +
+                ", Weight=" + calculateWeight();
+    }
+}
