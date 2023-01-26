d483bd85ed6f6777950c831f6d9e8118e7b274cf
Babiuc Ghenadie <babiuc.ghenadii@gmail.com>
2023-01-26 18:06:12 +0200

    exerciseII

--- Workshop-java/src/oop/lessonIII/exerciseII/Rectangle.java
+++ Workshop-java/src/oop/lessonIII/exerciseII/Rectangle.java
@@ -1,15 +1,18 @@
 package oop.lessonIII.exerciseII;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Rectangle {
     private double length;
     private double width;
+
+    public Rectangle(double... v) {
+        if (v.length == 1) {
+            this.length = v[0];
+            this.width = v[0];
+        } else if (v.length == 2) {
+            this.length = v[0];
+            this.width = v[1];
+        }
+    }
 
     public double calculateTheArea() {
         return length * width;
